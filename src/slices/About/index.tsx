4dc020c040dafc214e00f46d357a8a467d49ebcd
Fabian <acostahf4@gmail.com>
2023-10-16 11:08:54 -0500

    add in lottie file package

--- src/slices/About/index.tsx
+++ src/slices/About/index.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 /**
  * Props for `About`.
@@ -10,14 +12,30 @@ export type AboutProps = SliceComponentProps<Content.AboutSlice>;
  * Component for "About" Slices.
  */
 const About = ({ slice }: AboutProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for about (variation: {slice.variation}) Slices
-    </section>
-  );
+	return (
+		<section
+			data-slice-type={slice.slice_type}
+			data-slice-variation={slice.variation}
+			className="p-20"
+		>
+			<div className="grid grid-cols-2">
+				<div>
+					<h1 className="text-3xl text-white font-bold mb-4">About Me</h1>
+					<p className="text-white text-xl">
+						<PrismicRichText field={slice.primary.content} />
+					</p>
+				</div>
+				<div>
+					<Player
+						src="https://lottie.host/6cd08412-0153-4a3b-98a1-e7ddea0148ef/OM3FSuwJ6d.json"
+						autoplay
+						loop
+						style={{ height: "300px", width: "300px" }}
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
